35fe8a7be40fce5d6544573fda64434d5a095550
Lxghtend <lxghtend@gmail.com>
2026-05-11 09:22:50 -0400

    nix

--- modules/bash.nix
+++ modules/bash.nix
@@ -7,6 +7,7 @@
       home = "sudo -E nvim /etc/nixos/home.nix";
       config = "sudo -E nvim /etc/nixos/configuration.nix";
       nixd = "cd /etc/nixos";
+      svim = "sudo -E nvim";
       extract = "tar -xf";
       nerdfetch = "/etc/nixos/modules/nerdfetch";
     };
