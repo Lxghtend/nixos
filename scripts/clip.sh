eba4360321cfea76fc3919c778ce506b8e6800a6
Lxghtend <Lxghtend@gmail.com>
2026-06-27 18:51:59 -0400

    nix

--- scripts/clip.sh
+++ scripts/clip.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
+until [ -e /dev/input/event258 ]; do
+    echo "Waiting for /dev/input/event258..."
+    sleep 2
+done
 evtest /dev/input/event258 | while read line; do
     if echo "$line" | grep -q "BTN_MODE.*value 1"; then
         obs-cmd replay save
